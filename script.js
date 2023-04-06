0f3bf5094036efa1ce46c8203fa5c84b766e1348
Robert Hamilton <Robertmp1@icloud.com>
2023-04-06 10:49:30 -0400

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,6 @@
+const h2 = document.createElement("h2");
+const h3 = document.createElement("h3");
+h2.textContent = "This content added by JavaScript";
+h3.textContent= "Another content added by Javascript";
+document.querySelector("body").appendChild(h2);
+document.querySelector("body").appendChild(h3);
